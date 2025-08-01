c7fbbcdf4f337c750cab7689fdc38ce0fd0a2d32
Linus Tibert <git@linus.tibert.de>
2025-08-01 15:19:39 +0200

    rename Coordinate class to Vector2D
    It is more like a vector than a coordinate, although it is used for both things.

--- src/main.rs
+++ src/main.rs
@@ -10,26 +10,26 @@ const LINE_TOLERANCE_PX: f32 = 10.0;
 const ELLIPSE_CENTRUM_TOLERANCE_PX: i32 = 100;
 const ELLIPSE_TOLERANCE: f32 = 0.5;
 
-fn get_mouse_position() -> Coordinate {
+fn get_mouse_position() -> Vector2D {
     let position = Mouse::get_mouse_position();
     match position {
-        Mouse::Position { x, y } => Coordinate { x: x, y: y },
-        Mouse::Error => Coordinate { x: 0, y: 0 },
+        Mouse::Position { x, y } => Vector2D { x: x, y: y },
+        Mouse::Error => Vector2D { x: 0, y: 0 },
     }
 }
 
 #[derive(Clone, Default, PartialEq, Copy)]
-struct Coordinate {
+struct Vector2D {
     x: i32,
     y: i32,
 }
 
 #[derive(Clone, Default)]
 struct Recording {
-    coordinates: Vec<Coordinate>,
+    coordinates: Vec<Vector2D>,
     initialized: bool,
     running: bool,
-    stop_coordinate: Coordinate,
+    stop_coordinate: Vector2D,
     coordinate_unchanged_cycles: u8,
 }
 
@@ -50,7 +50,7 @@ enum ShapeName {
 
 #[derive(Clone, PartialEq)]
 struct Shape {
-    coordinates: Vec<Coordinate>,
+    coordinates: Vec<Vector2D>,
     shape_type: ShapeName,
  }
 
@@ -58,8 +58,8 @@ struct Shape {
 struct DistanceSet {
     min: i32,
     max: i32,
-    max_pair: [Coordinate; 2],
-    min_pair: [Coordinate; 2],
+    max_pair: [Vector2D; 2],
+    min_pair: [Vector2D; 2],
 }
 
 #[derive(Clone, PartialEq)]
@@ -71,8 +71,8 @@ struct PointDistanceSet {
     below: i32,
     values: i32,
     passes_percent: i32,
-    max_pair: [Coordinate; 2],
-    min_pair: [Coordinate; 2],
+    max_pair: [Vector2D; 2],
+    min_pair: [Vector2D; 2],
 }
 
 impl Recording {
@@ -88,7 +88,7 @@ impl Recording {
             self.init();
             println!("Initialized recording.");
         }
-        let current_mouse_coordinate: Coordinate = get_mouse_position();
+        let current_mouse_coordinate: Vector2D = get_mouse_position();
         if !(self.stop_coordinate == current_mouse_coordinate) {
             self.running = true;
             self.coordinates.push(current_mouse_coordinate.clone());
@@ -114,30 +114,30 @@ impl Recording {
     }
 }
 
-impl Coordinate {
+impl Vector2D {
     fn abs(&self) -> f32 {
         (((self.x.pow(2))+(self.y.pow(2))) as f32 ).sqrt()
     }
 
-    fn cross(self, other: Coordinate) -> i32 {
+    fn cross(self, other: Vector2D) -> i32 {
         self.x * other.y - self.y * other.x
     }
 
-    fn distance(&self, other: &Coordinate) -> i32 {
+    fn distance(&self, other: &Vector2D) -> i32 {
         let mut distance = (((self.x-other.x).pow(2)+(self.y-other.y).pow(2)) as f32).sqrt() as i32;
         distance = distance;
         distance
     }
 
-    fn distance_line(self, b: Coordinate, c: Coordinate) -> f32 {
-        let a: Coordinate = self;
-        let vector_ab: Coordinate = a-b;
-        let vector_ac: Coordinate = a-c;
+    fn distance_line(self, b: Vector2D, c: Vector2D) -> f32 {
+        let a: Vector2D = self;
+        let vector_ab: Vector2D = a-b;
+        let vector_ac: Vector2D = a-c;
         (vector_ab.cross(vector_ac) as f32)/(b-c).abs() as f32
     }
 }
 
-impl Sub for Coordinate {
+impl Sub for Vector2D {
     type Output = Self;
 
     fn sub(self, other: Self) -> Self::Output {
@@ -148,7 +148,7 @@ impl Sub for Coordinate {
     }
 }
 
-impl Add for Coordinate {
+impl Add for Vector2D {
     type Output = Self;
 
     fn add(self, other: Self) -> Self::Output {
@@ -159,7 +159,7 @@ impl Add for Coordinate {
     }
 }
 
-impl Div<i32> for Coordinate {
+impl Div<i32> for Vector2D {
     type Output = Self;
 
     fn div(self, other: i32) -> Self::Output {
@@ -170,7 +170,7 @@ impl Div<i32> for Coordinate {
     }
 }
 
-impl Mul<i32> for Coordinate {
+impl Mul<i32> for Vector2D {
     type Output = Self;
 
     fn mul(self, other: i32) -> Self::Output {
@@ -181,7 +181,7 @@ impl Mul<i32> for Coordinate {
     }
 }
 
-impl Mul for Coordinate {
+impl Mul for Vector2D {
     type Output = i32;
 
     fn mul(self, other: Self) -> Self::Output {
@@ -200,7 +200,7 @@ impl Shape {
             println!("CIRCLE ({}%)", passes_percent_circle);
             ShapeName::Circle
         } else if max_distance == start_end_distance {
-            let mut passed_coordinates: Vec<&Coordinate> = Vec::new();
+            let mut passed_coordinates: Vec<&Vector2D> = Vec::new();
             for coordinate in &self.coordinates {
                 let distance: f32 = coordinate.distance_line(self.coordinates[0], self.coordinates[self.coordinates.len()-1]);
                 if distance <= LINE_TOLERANCE_PX {
@@ -216,25 +216,25 @@ impl Shape {
                 ShapeName::Unknown
             }
         } else {
-            let max_pair: [Coordinate; 2] = self.get_distances().max_pair;
-            let longest_vector: Coordinate = max_pair[0] + max_pair[1];
-            let vector_centrum: Coordinate = longest_vector / 2;
-            let calculated_centrum: Coordinate = self.find_center();
+            let max_pair: [Vector2D; 2] = self.get_distances().max_pair;
+            let longest_vector: Vector2D = max_pair[0] + max_pair[1];
+            let vector_centrum: Vector2D = longest_vector / 2;
+            let calculated_centrum: Vector2D = self.find_center();
             if (calculated_centrum - vector_centrum).abs() as i32 <= ELLIPSE_CENTRUM_TOLERANCE_PX {
                 let check_point_amount: i32 = (self.coordinates.len()/2) as i32;
                 let mut last_distance: f32 = f32::MAX;
                 let mut grow: f32 = 0.0;
                 let mut shrink: f32 = 0.0;
-                let check_vectors: [Coordinate; 2] = [(calculated_centrum - max_pair[1]) / check_point_amount, (calculated_centrum - max_pair[0]) / check_point_amount];
+                let check_vectors: [Vector2D; 2] = [(calculated_centrum - max_pair[1]) / check_point_amount, (calculated_centrum - max_pair[0]) / check_point_amount];
                 let mut distance_errors: f32 = 0.0;
                 let mut distance_passed: f32 = 0.0;
-                let mut current_check_vector: Coordinate;
+                let mut current_check_vector: Vector2D;
                 for ii in 0..1 {
                     for i in 1..check_point_amount {
                         current_check_vector = vector_centrum + (check_vectors[ii] * i);
                         let distance_min: f32 = self.get_closest_to_point(current_check_vector).0.distance_line(max_pair[0], max_pair[1]).abs();
-                        let point_min: Coordinate = self.get_closest_to_point(current_check_vector).0;
-                        let mirrored_min: Coordinate = current_check_vector + (current_check_vector - point_min) * 2;
+                        let point_min: Vector2D = self.get_closest_to_point(current_check_vector).0;
+                        let mirrored_min: Vector2D = current_check_vector + (current_check_vector - point_min) * 2;
                         let mirrored_min_distance: f32 = self.get_closest_to_point(mirrored_min).0.distance_line(max_pair[0], max_pair[1]).abs();
                         if mirrored_min_distance - ELLIPSE_TOLERANCE * distance_min > distance_min || mirrored_min_distance + ELLIPSE_TOLERANCE * distance_min < distance_min {
                             distance_errors += 1.0;
@@ -267,8 +267,8 @@ impl Shape {
         }
     }
 
-    fn find_center(&self) -> Coordinate {
-        let mut average_coordinate: Coordinate = Coordinate { x: 0, y: 0 };
+    fn find_center(&self) -> Vector2D {
+        let mut average_coordinate: Vector2D = Vector2D { x: 0, y: 0 };
         for coordinate in &self.coordinates {
             average_coordinate.x += coordinate.x;
             average_coordinate.y += coordinate.y;
@@ -278,9 +278,9 @@ impl Shape {
         average_coordinate
     }
 
-    fn get_closest_to_point(&self, point: Coordinate) -> (Coordinate, i32) {
+    fn get_closest_to_point(&self, point: Vector2D) -> (Vector2D, i32) {
         let mut min_distance: i32 = i32::MAX;
-        let mut min_distance_point: Coordinate = Coordinate::default();
+        let mut min_distance_point: Vector2D = Vector2D::default();
         for other in &self.coordinates {
             if other != &point {
                 if point.distance(other) < min_distance {
@@ -295,8 +295,8 @@ impl Shape {
     fn get_distances(&self) -> DistanceSet {
         let mut max_distance: i32 = 0;
         let mut min_distance: i32 = i32::MAX;
-        let mut max_pair: [Coordinate; 2] = [Coordinate::default(), Coordinate::default()];
-        let mut min_pair:[Coordinate; 2] = max_pair.clone();
+        let mut max_pair: [Vector2D; 2] = [Vector2D::default(), Vector2D::default()];
+        let mut min_pair:[Vector2D; 2] = max_pair.clone();
         for point in &self.coordinates {
             for other in &self.coordinates {
                 if point != other {
@@ -314,13 +314,13 @@ impl Shape {
         DistanceSet { min: min_distance, max: max_distance, max_pair: max_pair, min_pair: min_pair }
     }
 
-    fn get_point_distances(&self, point: Coordinate) -> PointDistanceSet {
+    fn get_point_distances(&self, point: Vector2D) -> PointDistanceSet {
         let mut max_distance: i32 = 0;
         let mut min_distance: i32 = i32::MAX;
         let mut average: i32 = 0;
         let mut distances: Vec<i32> = Vec::new();
-        let mut max_pair: [Coordinate; 2] = [Coordinate::default(), Coordinate::default()];
-        let mut min_pair:[Coordinate; 2] = max_pair.clone();
+        let mut max_pair: [Vector2D; 2] = [Vector2D::default(), Vector2D::default()];
+        let mut min_pair:[Vector2D; 2] = max_pair.clone();
         for other in &self.coordinates {
             if point != *other {
                 let new_distance: i32 = point.distance(other);
